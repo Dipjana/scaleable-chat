9d6aa26db27596a614f96e8c44847e41e73e93f8
Dipjana <dipsundarjana@gmail.com>
2024-04-25 17:52:40 +0530

    first commit

--- README.md
+++ README.md
@@ -79,3 +79,8 @@ Learn more about the power of Turborepo:
 - [Filtering](https://turbo.build/repo/docs/core-concepts/monorepos/filtering)
 - [Configuration Options](https://turbo.build/repo/docs/reference/configuration)
 - [CLI Usage](https://turbo.build/repo/docs/reference/command-line-reference)
+
+
+## Turbo is a monorepo 
+
+Monorepo is a version-controlled code repository that hold many project 
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import {SocketProvider} from '../context/SocketProvider'
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +18,9 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
+      <SocketProvider>
       <body className={inter.className}>{children}</body>
+      </SocketProvider>
     </html>
   );
 }
