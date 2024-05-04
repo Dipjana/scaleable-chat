ebebb4b322fe478fb83b5e0ee93fa1664160d504
Dipjana <dipsundarjana@gmail.com>
2024-05-04 18:28:36 +0530

    test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,7 +7,6 @@ const { sendMessage, messages } = useSocket();
 const [message, setMessage] = useState('');
 
   return (<div>
-      
      <div>
       <input onChange={(e) => setMessage(e.target.value)} className={classes["chat-input"]} placeholder="Message..." />
       <button onClick={(e) => sendMessage(message)} className={classes["button"]}>Send</button>
